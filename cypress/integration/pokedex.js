ac9058359fc2e3c059d8de8f5c926fc698e073cb
TranLe2403 <trantran.0324@gmail.com>
2021-05-24 00:12:26 +0300

    Navigate to pokemon item

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,10 +1,15 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    const PORT = process.env.PORT || 5000
-    cy.visit(`http://localhost:${PORT}`)
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('Pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+
 })
