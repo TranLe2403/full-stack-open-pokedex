dbab4bc007540bb4fabc7b3abfb209b0b4fb6b8f
TranLe2403 <trantran.0324@gmail.com>
2021-05-23 14:55:37 +0300

    Fixed cypress CI fail

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,6 +1,7 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
+    const PORT = process.env.PORT || 5000
+    cy.visit(`http://localhost:${PORT}`)
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
